c2755f96082bb94a2cb38e308edacb105f437097
Jonathan Herman <jdh11235@gmail.com>
2015-01-18 17:53:01 -0600

    proper iteration in Marklets.loadAll()

--- app.js
+++ app.js
@@ -2,9 +2,22 @@
 
 
 var Marklets = {
+
+	load: function(marklet_template) {
+
+		console.log(marklet_template);
+
+	},
+
 	loadAll: function() {
 
+		var items = document.querySelectorAll('#marklets a');
+		for (var i = 0; i < items.length; i++) {
+			Marklets.load(items[i]);
+		}
+
 	}
+
 };
 
 
