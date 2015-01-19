b231af0588a42fbfa4f77fae06cba69150fedc53
Jonathan Herman <jdh11235@gmail.com>
2015-01-19 14:08:53 -0600

    file picker + drag-and-drop + new UI style

--- app.js
+++ app.js
@@ -61,9 +61,73 @@ var Prefs = {
 };
 
 
+var Uploader = {
+
+	process: function(file) {
+		if (file) {
+			console.log(file);
+			if (file.type === 'text/javascript') {
+				Uploader.$fileinfo.innerHTML = file.name;
+			} else {
+				alert('ERROR: ' + file.name + ' is ' + (file.type === '' ? 'unknown' : file.type) + ' - it should be JavaScript (.js)');
+			}
+
+		}
+	},
+
+	setup: function() {
+		Uploader.$filedrag = document.getElementById('filedrag');
+		Uploader.$filepicker = document.getElementById('filepicker');
+		Uploader.$fileinfo = document.getElementById('fileinfo');
+		Uploader.$filebutton = document.getElementById('filebutton');
+
+		Uploader.$filedrag.addEventListener('dragover', Uploader.fileDragoverHandler);
+		Uploader.$filedrag.addEventListener('dragleave', Uploader.fileDragleaveHandler);
+		Uploader.$filedrag.addEventListener('drop', Uploader.fileDropHandler);
+		Uploader.$filepicker.addEventListener('change', Uploader.filepickerHandler);
+		Uploader.$filebutton.addEventListener('click', Uploader.triggerfilepicker);
+	},
+
+	fileDropHandler: function(event) {
+		event.stopPropagation();
+		event.preventDefault();
+		event.target.className = '';
+		var file = event.dataTransfer.files[0];
+		Uploader.process(file);
+	},
+
+	filepickerHandler: function(event) {
+		var file = event.target.files[0];
+		Uploader.process(file);
+	},
+
+	fileDragoverHandler: function(event) {
+		event.stopPropagation();
+		event.preventDefault();
+		//PROBLEM: never contains any files
+//		var file = event.dataTransfer.files[0];
+//		event.target.className = (file.type === 'text/javascript' ? 'good' : 'bad');
+		Uploader.$filedrag.className = 'good';
+	},
+
+	fileDragleaveHandler: function(event) {
+		event.stopPropagation();
+		event.preventDefault();
+		event.target.className = '';
+	},
+
+	triggerfilepicker: function() {
+		Uploader.$filepicker.focus();
+		Uploader.$filepicker.click();
+	}
+
+};
+
+
 function init() {
 	Prefs.check();
 	Marklets.loadAll();
+	Uploader.setup();
 }
 
 
