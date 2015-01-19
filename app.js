2c5e991ce1938c9cbccf9d90c81cc33caa923e56
Jonathan Herman <jdh11235@gmail.com>
2015-01-18 18:56:09 -0600

    hey, sending the template to load2 callback works!

--- app.js
+++ app.js
@@ -3,14 +3,33 @@
 
 var Marklets = {
 
-	load: function(marklet_template) {
-		console.log(marklet_template);
+	load: function(template) {
+		var request = new XMLHttpRequest();
+		request.onload = function(event) {
+			Marklets.load_2(event, template);
+		};
+		request.open('get', template.href, true);
+		request.send();
+	},
+
+	load_2: function(event, template) {
+		var raw_marklet = event.currentTarget.responseText;
+		console.log(raw_marklet);
+		console.log(template);
+	},
+
+	convert: function(marklet) {
+		//input must always use semicolons and not have single-line comments
+		marklet = encodeURIComponent(marklet);
+		marklet = localStorage.marklet_prefix + marklet + localStorage.marklet_suffix;
+		return marklet;
 	},
 
 	loadAll: function() {
 		var items = document.querySelectorAll('#marklets a');
 		for (var i = 0; i < items.length; i++) {
 			var item = items[i];
+			//iterate through templates
 			Marklets.load(item);
 		}
 	}
