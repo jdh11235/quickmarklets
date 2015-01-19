782947a8a864d557754cfc3247c991d51ec8fd8d
Jonathan Herman <jdh11235@gmail.com>
2015-01-18 18:06:11 -0600

    Prefs class to manage localStorage

--- app.js
+++ app.js
@@ -4,24 +4,48 @@
 var Marklets = {
 
 	load: function(marklet_template) {
-
 		console.log(marklet_template);
-
 	},
 
 	loadAll: function() {
-
 		var items = document.querySelectorAll('#marklets a');
 		for (var i = 0; i < items.length; i++) {
-			Marklets.load(items[i]);
+			var item = items[i];
+			Marklets.load(item);
 		}
-
 	}
 
 };
 
 
+var Prefs = {
+
+	check: function() {
+		var items = Prefs.defaults;
+		for (var i = 0; i < items.length; i++) {
+			var item = items[i];
+			if (!localStorage.getItem(item.key)) {
+				localStorage.setItem(item.key, item.value);
+			}
+		}
+	},
+
+	defaults: [
+		{
+			key: 'key1',
+			value: 'value1'
+		},
+		{
+			key: 'key2',
+			value: 'value2'
+		}
+	]
+
+};
+
+
 function init() {
+	Prefs.check();
 	Marklets.loadAll();
 }
 
