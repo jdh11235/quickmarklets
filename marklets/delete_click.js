fdfe3e084febfb1039559bf18c555e0e7b2df791
Jonathan Herman <jdh11235@gmail.com>
2015-01-20 21:14:18 -0600

    add 'use strict' to all marklets

--- marklets/delete_click.js
+++ marklets/delete_click.js
@@ -1,7 +1,10 @@
+'use strict';
+
 function deleter(event) {
 	event.preventDefault();
 	var element = event.toElement;
 	element.parentNode.removeChild(element);
 	window.removeEventListener('click', deleter);
 }
+
 window.addEventListener('click', deleter);
--- marklets/hide_click.js
+++ marklets/hide_click.js
@@ -1,7 +1,10 @@
+'use strict';
+
 function hider(event) {
 	event.preventDefault();
 	var element = event.target;
 	element.setAttribute('hidden', '');
 	window.removeEventListener('click', hider);
 }
+
 window.addEventListener('click', hider);
