2528f712e10dba4d787c22b9df3870e992647a46
Jonathan Herman <jdh11235@gmail.com>
2015-01-20 21:05:09 -0600

    create Random Color marklet

---
+++ marklets/random_color.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function randRange(min, max) {
+	return Math.random() * (max - min) + min
+}
+function randInt(min, max) {
+	return Math.round(randRange(min, max))
+}
+function randIntString(min, max) {
+	return randInt(min, max).toString()
+}
+function randColor() {
+	return 'rgb(' + randIntString(0, 256) + ', ' + randIntString(0, 256) + ', ' + randIntString(0, 256) + ')'
+}
+
+window.addEventListener('click', function(event) {
+	var element = event.toElement;
+	element.style.backgroundColor = randColor();
+	element.style.color = randColor();
+});
