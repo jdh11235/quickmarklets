c0e1365b8ced57c9371bedff1c6f76e453c89bbd
Jonathan Herman <jdh11235@gmail.com>
2015-01-20 20:56:57 -0600

    add Rainbow marklet

---
+++ marklets/rainbow.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function randRange(min, max) {
+	return Math.random() * (max - min) + min
+}
+function randInt(min, max) {
+	return Math.round(randRange(min, max))
+}
+function randIntString(min, max) {
+	return randInt(min, max).toString()
+}
+function randColor() {
+	return 'rgb(' + randIntString(0, 256) + ', ' + randIntString(0, 256) + ', ' + randIntString(0, 256) + ')'
+}
+
+function rainbow(elementArray){
+	for (var i = 0; i < elementArray.length; i++) {
+		var elm = elementArray[i];
+		elm.style.backgroundColor = randColor();
+		elm.style.color = randColor();
+	}
+}
+
+rainbow(document.querySelectorAll('*'));
