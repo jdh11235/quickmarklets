4f57d5e1898bfd993312475e0891e1c2b33734df
Jonathan Herman <jdh11235@gmail.com>
2015-01-20 20:42:51 -0600

    add Delete Click marklet

---
+++ marklets/delete_click.js
@@ -0,0 +1,7 @@
+function deleter(event) {
+	event.preventDefault();
+	var element = event.toElement;
+	element.parentNode.removeChild(element);
+	window.removeEventListener('click', deleter);
+}
+window.addEventListener('click', deleter);
